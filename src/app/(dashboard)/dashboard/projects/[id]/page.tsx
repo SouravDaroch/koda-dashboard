18ff330d7dd67b7097742b025ec8326cfb88206c
SouravDaroch <souravdaroch984@gmail.com>
2026-03-06 03:24:55 +0530

    feat: add task filtering system

--- src/app/(dashboard)/dashboard/projects/[id]/page.tsx
+++ src/app/(dashboard)/dashboard/projects/[id]/page.tsx
@@ -22,9 +22,15 @@ export default function ProjectDetails({ params }: ProjectDetailsProps) {
         { id: "2", title: "Build dashboard UI", status: "In Progress" },
         { id: "3", title: "Connect API", status: "Todo" },
     ]);
-
+    // filter state 
+    const [filter, setFilter] = useState<"All" | "Todo" | "In Progress" | "Done">("All");
     // open add task modal 
     const [isOpen, setIsOpen] = useState(false);
+// FIltered tasks  
+    const filteredTasks =
+  filter === "All"
+    ? tasks
+    : tasks.filter((task) => task.status === filter);
 
     // Add task 
     const handleAddTask = (task: Task) => {
@@ -124,7 +130,27 @@ export default function ProjectDetails({ params }: ProjectDetailsProps) {
                     </button>
                 </div>
 
-                <TaskList tasks={tasks}
+{/* Filter Buttons UI */}
+<div className="flex gap-2">
+
+  {["All", "Todo", "In Progress", "Done"].map((status) => (
+    <button
+      key={status}
+      onClick={() => setFilter(status as any)}
+      className={`px-3 py-1 rounded-lg text-sm transition
+        ${
+          filter === status
+            ? "bg-violet-600 text-white"
+            : "bg-gray-100 text-gray-600 hover:bg-gray-200"
+        }`}
+    >
+      {status}
+    </button>
+  ))}
+
+</div>
+
+                <TaskList tasks={filteredTasks}
                     onDelete={handleDeleteTask}
                     onToggle={handleToggleStatus} />
             </div>
