f77d727c001803bd72d6ad3dd3920e3d10c11640
SouravDaroch <souravdaroch984@gmail.com>
2026-04-09 13:49:48 +0530

    feat: implement prisma 7 with neon and clerk user sync

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,20 +1,39 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import DashboardShell from "./components/DashboardShell";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { prisma } from "../../../../lib/prisma";
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
+  // Authenticate the session 
   const { userId } = await auth();
 
   if (!userId) {
     redirect("/sign-in");
   }
 
+  // Sync user to your Postgres Database
+  // We check if they exist; if not, we create them using data from Clerk
+  const existingUser = await prisma.user.findUnique({
+    where: { clerkId: userId },
+  });
+
+  if (!existingUser) {
+    const user = await currentUser();
+
+    if (user) {
+      await prisma.user.create({
+        data: {
+          clerkId: userId,
+          email: user.emailAddresses[0].emailAddress,
+        },
+      });
+    }
+  }
   return <DashboardShell>{children}
-  <SpeedInsights/></DashboardShell>;
+    <SpeedInsights /></DashboardShell>;
 }
